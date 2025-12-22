dca5021a1d960b74cf157d4d1f0e5b33e5e5aa03
fangyang <fangyang1@xiaoyouzi.com>
2025-12-22 18:19:45 +0800

    feat: 修改包

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import { useState } from "react";
 import MarkdownEditor from "./MarkdownEditor";
 
 export default function App() {
-  const [md, setMd] = useState(`# Hello Markdown
-
-这里是一个 **Monaco Markdown 编辑器**
-
+  const [md, setMd] = useState(`# Hello Markdown这里是一个 **Monaco Markdown 编辑器**
 - 支持光标恢复
 - 支持大文档
 - VS Code 同款体验

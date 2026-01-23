f4a2738c0fb48c3287cb1bddf53aac94cbadef85
fangyang <fangyang1@xiaoyouzi.com>
2026-01-23 14:42:54 +0800

    feat: 错误修改

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 // @ts-ignore
 import App from "./App.jsx";
